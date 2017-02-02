fe8d4fba845be18f324d55bd305db679dea4ba68
henry_pheng <henry.pheng@gmail.com>
2017-02-02 22:39:21 +0100

    04 - firebase & ghosts

---
+++ js/04-main.js
@@ -0,0 +1,137 @@
+(function () {
+    //-- FIREBASE
+    // Initialize Firebase
+    let config = {
+        apiKey: "AIzaSyC99m083bi3ST6Cuu7GJP2QOM2dk-G234U",
+        authDomain: "b3-js-b50d5.firebaseapp.com",
+        databaseURL: "https://b3-js-b50d5.firebaseio.com",
+        storageBucket: "b3-js-b50d5.appspot.com",
+        messagingSenderId: "568707431220"
+    };
+    let mainFirebaseApp = firebase.initializeApp(config);
+
+    //--  worker
+    let worker = new Worker("js/04-worker.js");
+
+    //--  event trigger pour alimenter le worker -----------------------------------------------------------------------
+    let moveRequestTimeout;
+    $("#viewport").on('keydown keyup mousemove', function (e) {
+        // une demande de deplacement
+        // si une demande de reinitialisation de deplacement avec delai avait été lancée on la stoppe
+        clearTimeout(moveRequestTimeout);
+
+        let moveRequest = {
+            top: 0,
+            left: 0
+        };
+
+        if (e.type == "keydown" || e.type == "keyup") {
+            console.log(e.type);
+            console.log(moveRequest);
+            if ([37, 38, 39, 40].indexOf(e.which) != -1) {
+
+                switch (e.which) { // ou e.keyCode
+                    case 37:
+                        moveRequest.left = (e.type == "keydown") ? 1 : 0;
+                        break;
+                    case 38:
+                        moveRequest.top = (e.type == "keydown") ? 1 : 0;
+                        break;
+                    case 39:
+                        moveRequest.left = (e.type == "keydown") ? -1 : 0;
+                        break;
+                    case 40:
+                        moveRequest.top = (e.type == "keydown") ? -1 : 0;
+                        break;
+                }
+            }
+        }
+        if (e.type == "mousemove") {
+            // essaie
+            try {
+                moveRequest.left = (e.offsetX > $(".player").first().position().left) ? -1 : 1;
+                moveRequest.top = (e.offsetY > $(".player").first().position().top) ? -1 : 1;
+                // si une erreur on la traite
+            } catch (exception) {
+                console.error("mousemove exception", exception);
+                // au final on execute
+            } finally {
+                // on defini un deplacement avec delai
+                moveRequestTimeout = setTimeout(function () {
+                    worker.postMessage({
+                        top: 0,
+                        left: 0
+                    });
+                }, 100);
+            }
+        }
+        // on envoie l'object moveRequest au worker
+        worker.postMessage(moveRequest);
+    });
+
+
+    let entities = [];
+
+
+    //-- event trigger sur les messages retour du worker au travers de l'evenement "message" ---------------------------
+    worker.onmessage = function (event) {
+        // l'evenement est retourné, pour recup l'object associe on appelle event.data
+        entities = event.data;
+
+
+        /*
+         // on peut utiliser plusieurs approches pour itérer (boucles)
+         // -- FOR
+         //le classique => for(let i=0; i< max; i++) { i }
+
+         // -- FOR-OF
+         // pour recup les *valeurs* d'un array ou d'une liste => for (let valeur of tableau) { valeur }
+         // pour recup les *propriétés* d'un object => for (let nomPropriété of Object.keys(monObjet)) { nomPropriété }
+         // pour recup les valeur de *propriétés* d'un object => for (let nomPropriété of Object.keys(monObjet)) { monObjet[nomPropriété] }
+
+         // -- FOR-IN => que pour les objects
+         // pour recup les *propriétés* d'un object => for (let nomPropriété in monObjet) { nomPropriété }
+         // pour recup les valeur de *propriétés* d'un object =>   for (let nomPropriété in monObjet) { monObjet[nomPropriété] }
+         */
+
+
+        for (let entity of entities) {
+            if (entity.class == "player") {
+                entity.ts = Date.now();
+                mainFirebaseApp.database().ref('players/' + entity.id)
+                    .set(entity)
+                    .then(function (e) {
+                    });
+            }
+        }
+
+    };
+
+    worker.onerror = function (error) {
+        console.error("Worker error: " + error.message + "\n");
+        throw error;
+    };
+
+    mainFirebaseApp.database().ref('players/').orderByChild("ts").limitToLast(2)
+        .on('value', function (snapshot) {
+            let ghosts = [];
+            let firebasePlayers = [];
+            for (let entityId in snapshot.val()) {
+                let ghost = snapshot.val()[entityId];
+                ghost.class = "ghost";
+                ghosts.push(ghost);
+            }
+            let all =  entities.concat(ghosts);
+            for (let entity of all) {
+                let currentEntity = $("#" + entity.id);
+                if (currentEntity.length == 0) {
+                    $('#map').append($('<div id="' + entity.id + '" class="' + entity.class + '">'));
+                }
+                currentEntity.removeClass('left right top bottom nearby');
+                currentEntity.addClass(entity.angle);
+                currentEntity.addClass((entity.isNearby) ? 'nearby' : '');
+                currentEntity.css('transform', 'translate(' + entity.x * 8 + 'px,' + entity.y * 8 + 'px)');
+            }
+        });
+
+})(jQuery);
---
+++ js/04-worker.js
@@ -0,0 +1,129 @@
+class Entity {
+
+    constructor(className, posX, posY) {
+        this.class = className;
+        this.position = [posX, posY];
+        this.id = "Entity" + Math.round(Math.random() * 65000);
+
+    }
+
+    set position(position) {
+        this.x = position[0];
+        this.y = position[1];
+    }
+
+    get position() {
+        return ([this.x, this.y]);
+    }
+    moveBy(moveRequest){
+        this.x -= moveRequest.left;
+        this.y -= moveRequest.top;
+
+        this.x = Math.max(Math.min(this.x, 640/8), 0);
+        this.y = Math.max(Math.min(this.y, 480/8), 0);
+
+
+        if (moveRequest.left < 0) {
+            this.bearing ='right';
+        }
+        if (moveRequest.left > 0) {
+            this.bearing ='left';
+        }
+        if (moveRequest.top < 0) {
+            this.bearing ='bottom';
+        }
+        if (moveRequest.top > 0) {
+            this.bearing ='top';
+        }
+    }
+
+    set bearing(bearing) {
+        this.angle = bearing;
+    }
+
+    get bearing() {
+        return (this.angle);
+    }
+
+
+}
+
+// on etend la classe et on lui declare une nouvelle methode move qui le deplacera aleatoirement.
+class Enemy extends Entity{
+    moveBy(){
+        this.x += Math.round(Math.random()*2) -1;
+        this.y += Math.round(Math.random()*2) -1;
+
+        this.x = Math.max(Math.min(this.x, 640/8), 0);
+        this.y = Math.max(Math.min(this.y, 480/8), 0);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+let moveRequest = {
+    left: 0,
+    top: 0
+};
+
+// on créée le tableau des entités
+let entities = [];
+// hop on ajoute le player
+entities.push(new Entity('player', 5, 5));
+// hop on ajoute des enemies dans le tableau
+for (let i=0; i < 5; i++){
+    entities.push(new Enemy('enemy', Math.round(Math.random()*640/8), Math.round(Math.random()*480/8)));
+}
+
+// le tick
+onmessage = function (event) {
+    //console.log(event.data);
+    moveRequest = event.data;
+    //gameTick();
+};
+
+
+let isNearby = function(entity) {
+    switch(entity.class){
+        case "player":
+            break;
+            return true;
+        case "enemy":
+            entity.isNearby = (Math.abs(entity.x - entities[0].x) <= 3 && Math.abs(entity.y - entities[0].y) <= 3);
+            return entity.isNearby;
+            break;
+    }
+
+};
+
+let isOnPlayer = function(entity) {
+    switch(entity.class){
+        case "player":
+            return true;
+            break;
+        case "enemy":
+            return !(Math.abs(entity.x - entities[0].x) <= 1 && Math.abs(entity.y - entities[0].y) <= 1);
+            break;
+    }
+};
+
+let gameTick = function () {
+    //console.log( moveRequest);
+
+    for (let entity of entities){
+        switch(entity.class){
+            case "player":
+                entity.moveBy(moveRequest);
+                break;
+            case "enemy":
+                entity.moveBy();
+                break;
+        }
+    }
+    // highlight si proche
+    entities.filter(isNearby);
+    //supprime si sur le player
+    entities = entities.filter(isOnPlayer);
+    postMessage(entities);
+};
+
+self.setInterval(gameTick, 16);
